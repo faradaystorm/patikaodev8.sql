0299d4d1e0fd5055c6860163def46a8cbbf90d19
Esma Şen <96180440+faradaystorm@users.noreply.github.com>
2022-07-23 13:31:34 +0300

    Create odev1.1.sql

---
+++ odev1.1.sql
@@ -0,0 +1,7 @@
+--test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
+CREATE TABLE employee (
+id INT NOT NULL,
+name VARCHAR(50) NOT NULL,
+birthday DATE,
+email VARCHAR(100)
+);
